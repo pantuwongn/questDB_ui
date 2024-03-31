a5c511ee7f51092828ea0c43475578b2b3a4599d
Natapon Pantuwong <pantuwongn@gmail.com>
2024-03-31 11:35:44 +0700

    add ExportCSVDialog

---
+++ packages/web-console/src/components/ExportCSVDialog/actions.tsx
@@ -0,0 +1,19 @@
+import React from "react"
+import { Box } from "../Box"
+import { FileExcel2 as FileIcon } from "@styled-icons/remix-line"
+import { Form } from "../Form"
+import type { Action, SchemaColumn } from "./types"
+
+export const Actions = ({
+  ctaText,
+}: {
+  ctaText: string
+}) => {
+  return (
+    <Box gap="1rem">
+      <Form.Submit prefixIcon={<FileIcon size={18} />} variant="success">
+        {ctaText}
+      </Form.Submit>
+    </Box>
+  )
+}
